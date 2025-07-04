9b0a9905a55428631e48b1f5d2c2f1f1eb59e30c
SOUVIK SAHA <souvik21102001@gmail.com>
2025-07-04 23:56:03 +0530

    index.js updated

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res)=> {
-    res.send("Welcome to this Server");
-});
+
 
 app.listen(port, ()=> {
     console.log("Server is running on port ",port);
