2197adc0cdfe8246f2b7dd9c4ba387ef6cb3169c
SOUVIK SAHA <souvik21102001@gmail.com>
2025-07-21 23:51:54 +0530

    incomplete authentication

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+import User from "../models/user.model.js";
+
+export const signUp = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const existEmail = await User.findOne({ email });
+        if (existEmail) {
+            return res.status(400).json({ message: "Email already exists." });
+        }
+        if(password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters" });
+        }
+    } catch (error) {
+        
+    }
+}
