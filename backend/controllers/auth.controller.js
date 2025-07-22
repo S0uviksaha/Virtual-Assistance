135da00c7b48af0f053f552ef0b7e81b30cdca46
SOUVIK SAHA <souvik21102001@gmail.com>
2025-07-22 21:04:54 +0530

    Sign Up

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,16 +1,37 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 export const signUp = async (req, res) => {
     try {
         const { name, email, password } = req.body;
-
+        if (!name || !email || !password) {
+            console.log("Error due to missing fields in signup. name: ", name, " email: ", email, " password: ", password);
+            console.log("Fill all fields.");
+            return res.status(400).json({ message: "All fields are required." });
+        }
         const existEmail = await User.findOne({ email });
         if (existEmail) {
+            console.log("Error due to existing email in signup. email: ", email);
+            console.log("Enter a different email.");
             return res.status(400).json({ message: "Email already exists." });
         }
         if(password.length < 6) {
+            console.log("Error due to short password in signup. Password length: ", password.length);
+            console.log("Password must be at least 6 characters.");
             return res.status(400).json({ message: "Password must be at least 6 characters" });
         }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        console.log("User created successfully. User : ", user);
+        return res.status(201).json({ message: "User created successfully", user });
+
     } catch (error) {
         
     }
--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,10 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
+
 app.listen(port, ()=> {
     connectDB();
     console.log("Server is running on port ",port);
